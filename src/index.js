d0046be5dc6f9ccc5b3ea22bee92b48e445cfc25
yashmathur865 <yashmathur865@gmail.com>
2024-04-02 16:16:05 +0530

    index.js added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const app = express();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// middleware 
+app.use(express.json());
+
+const blog = require('./routes/blog');
+
+// mount 
+app.use("/api/v1",blog);
+
+const dbConnect = require('./config/database');
+dbConnect();
+
+// Start Server 
+app.listen(PORT,()=>{
+    console.log("App is Running at the",PORT);
+})
+
+// Default Route 
+app.get('/', (req,res) => {
+    res.send(`<h1>HomePage</h1>`)
+})
